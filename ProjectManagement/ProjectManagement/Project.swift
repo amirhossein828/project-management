aad18807423f85446b011f192eb01c976645584f
Amir <hashemi.eng1985@gmail.com>
2017-08-06 16:05:50 -0400

    add realm pods. rearrange project

---
+++ ProjectManagement/ProjectManagement/Project.swift
@@ -0,0 +1,9 @@
+//
+//  Project.swift
+//  ProjectManagement
+//
+//  Created by seyedamirhossein hashemi on 2017-08-06.
+//  Copyright © 2017 seyedamirhossein hashemi. All rights reserved.
+//
+
+import Foundation
