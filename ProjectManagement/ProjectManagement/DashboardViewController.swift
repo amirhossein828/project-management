0e746da5c5e16df8276352cffe4f0f955a413018
Amir <hashemi.eng1985@gmail.com>
2017-08-06 17:58:37 -0400

    add collection to dashboard view controller. populate cell for collection view.

--- ProjectManagement/ProjectManagement/DashboardViewController.swift
+++ ProjectManagement/ProjectManagement/DashboardViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
+private let reuseIdentifier = "Cell"
+
 class DashboardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+  
 
 
+}
+
+extension DashboardViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    // MARK: UICollectionViewDataSource
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+        // Configure the cell
+        
+        return cell
+    }
+    
 }
 
