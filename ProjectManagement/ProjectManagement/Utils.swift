8aa3f27d29db63320d9d10afec5e6090919d1f0e
Amir <hashemi.eng1985@gmail.com>
2017-08-06 22:58:48 -0400

    add Utils class to add utility functions. add dateToString and dateFromString.

--- ProjectManagement/ProjectManagement/Utils.swift
+++ ProjectManagement/ProjectManagement/Utils.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+// convert string to date
+ func dateFromString(_ dateAsString: String?) -> Date? {
+    guard let string = dateAsString else { return nil }
+    
+    let dateformatter = DateFormatter()
+    dateformatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSSS"
+    let val = dateformatter.date(from: string)
+    return val
+}
+
+// convert date to String
+ func dateToString(_ dateIn: Date?) -> String? {
+    guard let date = dateIn else { return nil }
+    let dateformatter = DateFormatter()
+    dateformatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSSS"
+    let val = dateformatter.string(from: date)
+    return val
+}
