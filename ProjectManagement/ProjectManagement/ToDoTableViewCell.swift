1e20b0ddf3b900fb6fcd7976f0b91bcdd97e21ef
Amir <hashemi.eng1985@gmail.com>
2017-08-08 16:54:02 -0400

    add protocol to implementation of delegation pattern to add button in the cell of table view.(protocol with two method for changing the toDo to inProgress for task stutus) . and also add the property delegate.

--- ProjectManagement/ProjectManagement/ToDoTableViewCell.swift
+++ ProjectManagement/ProjectManagement/ToDoTableViewCell.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 class ToDoTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var goInProgressButton: UIButton!
+    @IBOutlet weak var nameOfTaskLabel: UILabel!
+    
+    weak var delegate : ToDoCellDelegate?
 
+    @IBAction func changeStutusToInProgress(_ sender: UIButton) {
+        
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
+}
+
+
+// protocol for delegation of some method to ToDoTableViewController
+protocol ToDoCellDelegate : class{
+    // method to change the stutus of task form toDo to inProgress
+    func changeStutusOfTask(forProject : Project)
+    // method to delete toDoTask from taskArray when the changeStutusToInProgress tapped
+    func deleteFromArray(withIndex : IndexPath)
+    
 }
