d90b3d89844299c91f92e13a99940dc55e66c57e
Amir <hashemi.eng1985@gmail.com>
2017-08-08 19:20:02 -0400

    add toDoCell class with 3 property and button

--- ProjectManagement/ProjectManagement/ToDoTableViewCell.swift
+++ ProjectManagement/ProjectManagement/ToDoTableViewCell.swift
@@ -14,10 +14,23 @@ class ToDoTableViewCell: UITableViewCell {
     @IBOutlet weak var nameOfTaskLabel: UILabel!
     
     weak var delegate : ToDoCellDelegate?
-
+    var taskSelected : Task!
+    var project : Project!
+    var index  : IndexPath!
+    
     @IBAction func changeStutusToInProgress(_ sender: UIButton) {
-        
+        delegate?.changeStutusOfTask(forProject: project, forTask : taskSelected)
+        delegate?.deleteFromArray(withIndex: index)
     }
+    
+    // set label and properties of cell from table view
+    func setLabelAndCell(withTask taskFromToDo : Task, withProject projectFromToDo : Project, index  :IndexPath){
+        self.project = projectFromToDo
+        self.taskSelected = taskFromToDo
+        self.index = index
+        self.nameOfTaskLabel.text = self.taskSelected.nameOfTask
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
@@ -32,7 +45,7 @@ class ToDoTableViewCell: UITableViewCell {
 // protocol for delegation of some method to ToDoTableViewController
 protocol ToDoCellDelegate : class{
     // method to change the stutus of task form toDo to inProgress
-    func changeStutusOfTask(forProject : Project)
+    func changeStutusOfTask(forProject : Project, forTask : Task)
     // method to delete toDoTask from taskArray when the changeStutusToInProgress tapped
     func deleteFromArray(withIndex : IndexPath)
     
