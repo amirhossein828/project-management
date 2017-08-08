2575f5103f8285a23d04a793eacb841ba0b6789f
Amir <hashemi.eng1985@gmail.com>
2017-08-07 20:45:38 -0400

    add to method to calculate number of todo and inprogress and done task of the project. and calculate time between two date.

--- ProjectManagement/ProjectManagement/Project.swift
+++ ProjectManagement/ProjectManagement/Project.swift
@@ -8,7 +8,6 @@
 
 import Foundation
 import RealmSwift
-import ObjectMapper
 
 
 
@@ -19,8 +18,32 @@ class Project : Object {
     dynamic var finishingDate : Date?
     var tasks = List<Task>()
     
+    
     override class func primaryKey() -> String {
         return "projectId"
     }
     
+    // calculate the number of toDo and inProgress and done tasks
+    func calculateNumberOfToDoInProgressDoneTasks() -> [Int]{
+        var taskStatusArray :  [Int]?
+        var numberOfToDo = 0
+        var numberOfInProgress = 0
+        var numberofDone = 0
+        for task in tasks {
+            if task.status == .TODO {
+                numberOfToDo += 1
+            }else if task.status == .INPROGRESS {
+                numberOfInProgress += 1
+            }else {
+                numberofDone += 1
+            }
+        }
+        taskStatusArray = [numberOfToDo,numberOfInProgress,numberofDone]
+        return taskStatusArray!
+    }
+    // calculate time left to finish
+    func dateDuration() -> TimeInterval{
+        var durationDate = DateInterval(start: startingDate!, end: finishingDate!)
+        return durationDate.duration
+    }
 }
