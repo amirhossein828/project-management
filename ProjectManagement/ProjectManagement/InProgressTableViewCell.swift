59873c37307d193ffa97b454e65e0d8587ca337d
Amir <hashemi.eng1985@gmail.com>
2017-08-08 20:10:35 -0400

    add cell class for InProgress table view. with same cell in ToDocCell

--- ProjectManagement/ProjectManagement/InProgressTableViewCell.swift
+++ ProjectManagement/ProjectManagement/InProgressTableViewCell.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class InProgressTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var nameOfInProgressTsksLabel: UILabel!
+    
+    weak var delegate : ToDoCellDelegate?
+    var taskSelected : Task!
+    var project : Project!
+    var index  : IndexPath!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+    
+    // set label and properties of cell from table view
+    func setLabelAndCell(withTask taskFromToDo : Task, withProject projectFromToDo : Project, index  :IndexPath){
+        self.project = projectFromToDo
+        self.taskSelected = taskFromToDo
+        self.index = index
+        self.nameOfInProgressTsksLabel.text = self.taskSelected.nameOfTask
+    }
 
+    @IBAction func goToDoneTasksButton(_ sender: UIButton) {
+        delegate?.changeStutusOfTask(forProject: project, forTask : taskSelected)
+        delegate?.deleteFromArray(withIndex: index)
+    }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
@@ -22,3 +41,5 @@ class InProgressTableViewCell: UITableViewCell {
     }
 
 }
+
+
